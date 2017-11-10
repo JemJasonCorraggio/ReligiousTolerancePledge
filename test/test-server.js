eecb6741e7ebed34e230f2f7dce4e713350fbf2d
Jem Corraggio <ladyjem@gmail.com>
2017-11-10 18:21:19 +0000

    nav bar

--- test/test-server.js
+++ test/test-server.js
@@ -12,8 +12,23 @@ describe('GET', function() {
     after(function() {
         return closeServer();
     });
-            it('Should return status 200', function() {
+            it('Should return status 200 when accessing index.html', function() {
                 return chai.request(app).get('/').then(res => {
+                    expect(res).to.have.status(200);
+                });
+            });
+            it('Should return status 200 when accessing why.html', function() {
+                return chai.request(app).get('/why.html').then(res => {
+                    expect(res).to.have.status(200);
+                });
+            });
+            it('Should return status 200 when accessing list.html', function() {
+                return chai.request(app).get('/list.html').then(res => {
+                    expect(res).to.have.status(200);
+                });
+            });
+            it('Should return status 200 when accessing ask.html', function() {
+                return chai.request(app).get('/ask.html').then(res => {
                     expect(res).to.have.status(200);
                 });
             });
