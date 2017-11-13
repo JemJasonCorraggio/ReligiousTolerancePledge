2940860f52a6113c2a265dfe5ee87508cf7dbdb2
Jem Corraggio <ladyjem@gmail.com>
2017-11-13 02:44:20 +0000

    solution?

--- Public/app.js
+++ Public/app.js
@@ -96,7 +96,7 @@ $(".signup").submit(function(event) {
 
         $.ajax("https://religious-tolerance-pledge.herokuapp.com/businesses",{
             method: "POST",
-           data: newBusiness,
+           data: {body: newBusiness},
            success: function (token) { localStorage.setItem("token", token); return window.location.href = "certificate.html"; },
            error: function(type, error) { $(".error").removeClass("hidden");
       return $(".error").html(error||"server error");}
