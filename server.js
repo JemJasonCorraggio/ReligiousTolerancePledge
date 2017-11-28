63e373d5b46a938e0e577bfc05eb2d8ccb782086
Jem Corraggio <ladyjem@gmail.com>
2017-11-28 21:08:45 +0000

    CORS

--- server.js
+++ server.js
@@ -78,7 +78,12 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 
 mongoose.Promise = global.Promise;
-
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  next();
+});
 app.post(
     '/login',
     // The user provides a username and password to login
